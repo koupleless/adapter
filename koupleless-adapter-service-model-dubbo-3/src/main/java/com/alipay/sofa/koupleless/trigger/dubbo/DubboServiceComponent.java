6a5515afb88385fe18a0835abeabe8f17efc4798
gaosaroma <gaosaroma@gmail.com>
2024-05-21 13:56:46 +0800

    add service-model for dubbo3.0.1+

---
+++ koupleless-adapter-service-model-dubbo-3/src/main/java/com/alipay/sofa/koupleless/trigger/dubbo/DubboServiceComponent.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.koupleless.trigger.dubbo;
+
+import com.alipay.sofa.koupleless.common.service.AbstractServiceComponent;
+import com.alipay.sofa.koupleless.common.service.ServiceState;
+import lombok.Builder;
+
+import static com.alipay.sofa.koupleless.trigger.dubbo.KouplelessDubboServiceConstants.PROTOCOL;
+
+/**
+ * @author lianglipeng.llp@alibaba-inc.com
+ * @version $Id: DubboServiceComponent.java, v 0.1 2024年05月20日 14:19 立蓬 Exp $
+ */
+
+public class DubboServiceComponent extends AbstractServiceComponent {
+    private final String dubboProtocol;
+
+    @Builder
+    public DubboServiceComponent(String dubboProtocol, String identifier, Object bean,
+                                 Class<?> beanClass, Class<?> interfaceType, Object metaData,
+                                 ServiceState state) {
+        super(PROTOCOL, identifier, bean, beanClass, interfaceType, metaData, state);
+        this.dubboProtocol = dubboProtocol;
+    }
+
+    public String getDubboProtocol() {
+        return dubboProtocol;
+    }
+}
---
+++ koupleless-adapter-service-model-dubbo-3/src/main/java/com/alipay/sofa/koupleless/trigger/dubbo/KouplelessDubboServiceListener.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.koupleless.trigger.dubbo;
+
+import com.alipay.sofa.koupleless.common.BizRuntimeContext;
+import com.alipay.sofa.koupleless.common.BizRuntimeContextRegistry;
+import com.alipay.sofa.koupleless.common.exception.BizRuntimeException;
+import com.alipay.sofa.koupleless.common.service.AbstractServiceComponent;
+import com.alipay.sofa.koupleless.common.service.ServiceState;
+import org.apache.dubbo.config.ServiceConfig;
+import org.apache.dubbo.config.ServiceListener;
+import org.apache.dubbo.rpc.model.ServiceMetadata;
+
+import static com.alipay.sofa.koupleless.common.exception.ErrorCodes.ServiceManager.E200001;
+import static com.alipay.sofa.koupleless.common.exception.ErrorCodes.SpringContextManager.E100002;
+import static com.alipay.sofa.koupleless.trigger.dubbo.KouplelessDubboServiceConstants.PROTOCOL;
+import static com.alipay.sofa.koupleless.trigger.dubbo.KouplelessDubboUtils.buildServiceIdentifier;
+import static com.alipay.sofa.koupleless.trigger.dubbo.KouplelessDubboUtils.parseDubboProtocol;
+
+/**
+ * @author lianglipeng.llp@alibaba-inc.com
+ * @version $Id: KouplelessDubboServiceListener.java, v 0.1 2024年05月20日 14:15 立蓬 Exp $
+ */
+public class KouplelessDubboServiceListener implements ServiceListener {
+
+    @Override
+    public void exported(ServiceConfig sc) {
+        BizRuntimeContext bizRuntimeContext = BizRuntimeContextRegistry
+            .getBizRuntimeContextByClassLoader(Thread.currentThread().getContextClassLoader());
+        if (bizRuntimeContext == null) {
+            throw new BizRuntimeException(E100002, "biz runtime context is null");
+        }
+
+        ServiceMetadata serviceMetadata = sc.getServiceMetadata();
+        Object target = serviceMetadata.getTarget();
+        DubboServiceComponent dubboServiceComponent = DubboServiceComponent.builder()
+            .dubboProtocol(parseDubboProtocol(sc)).identifier(buildServiceIdentifier(sc))
+            .bean(target).beanClass(target.getClass())
+            .interfaceType(serviceMetadata.getServiceType()).metaData(serviceMetadata)
+            .state(ServiceState.EXPORTED).build();
+        bizRuntimeContext.registerService(dubboServiceComponent);
+    }
+
+    @Override
+    public void unexported(ServiceConfig sc) {
+        BizRuntimeContext bizRuntimeContext = BizRuntimeContextRegistry
+            .getBizRuntimeContextByClassLoader(Thread.currentThread().getContextClassLoader());
+        if (bizRuntimeContext == null) {
+            throw new BizRuntimeException(E100002, "biz runtime context is null");
+        }
+        AbstractServiceComponent component = bizRuntimeContext.getServiceComponent(PROTOCOL,
+            buildServiceIdentifier(sc));
+        if (component == null) {
+            throw new BizRuntimeException(E200001,
+                "service not found:" + buildServiceIdentifier(sc));
+        }
+        component.setServiceState(ServiceState.UNEXPORTED);
+        bizRuntimeContext.unregisterService(component);
+    }
+}
