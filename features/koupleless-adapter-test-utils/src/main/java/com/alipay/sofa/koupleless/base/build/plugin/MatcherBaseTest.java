6a335e328860907ed0571b7695c14e76d0b9806e
lvjing2 <leojames.googol@gmail.com>
2025-02-27 14:39:16 +0800

    add adpater mapping valid check

--- features/koupleless-adapter-test-utils/src/main/java/com/alipay/sofa/koupleless/base/build/plugin/MatcherBaseTest.java
+++ features/koupleless-adapter-test-utils/src/main/java/com/alipay/sofa/koupleless/base/build/plugin/MatcherBaseTest.java
@@ -20,6 +20,7 @@ package com.alipay.sofa.koupleless.base.build.plugin;
 import com.alipay.sofa.koupleless.base.build.plugin.model.KouplelessAdapterConfig;
 import com.alipay.sofa.koupleless.base.build.plugin.model.MavenDependencyAdapterMapping;
 import lombok.SneakyThrows;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
@@ -27,31 +28,30 @@ import org.eclipse.aether.DefaultRepositorySystemSession;
 import org.eclipse.aether.RepositorySystem;
 import org.eclipse.aether.RepositorySystemSession;
 import org.eclipse.aether.artifact.DefaultArtifact;
-import org.eclipse.aether.collection.CollectRequest;
-import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory;
-import org.eclipse.aether.graph.DependencyFilter;
-import org.eclipse.aether.impl.DefaultServiceLocator;
 import org.eclipse.aether.repository.LocalRepository;
 import org.eclipse.aether.repository.RemoteRepository;
 import org.eclipse.aether.resolution.ArtifactRequest;
 import org.eclipse.aether.resolution.ArtifactResolutionException;
 import org.eclipse.aether.resolution.ArtifactResult;
-import org.eclipse.aether.resolution.DependencyRequest;
-import org.eclipse.aether.resolution.DependencyResolutionException;
-import org.eclipse.aether.resolution.DependencyResult;
 import org.eclipse.aether.resolution.VersionRangeRequest;
-import org.eclipse.aether.resolution.VersionRangeResolutionException;
 import org.eclipse.aether.resolution.VersionRangeResult;
-import org.eclipse.aether.spi.connector.RepositoryConnectorFactory;
-import org.eclipse.aether.spi.connector.transport.TransporterFactory;
-import org.eclipse.aether.transport.file.FileTransporterFactory;
-import org.eclipse.aether.transport.http.HttpTransporterFactory;
-import org.eclipse.aether.util.filter.DependencyFilterUtils;
+import org.eclipse.aether.supplier.RepositorySystemSupplier;
 import org.eclipse.aether.version.InvalidVersionSpecificationException;
 import org.eclipse.aether.version.Version;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.diff.DiffFormatter;
+import org.eclipse.jgit.diff.RawTextComparator;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.treewalk.CanonicalTreeParser;
+import org.eclipse.jgit.treewalk.FileTreeIterator;
+import org.junit.Assert;
+import org.junit.Test;
 import org.mockito.Mockito;
 import org.yaml.snakeyaml.Yaml;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -59,17 +59,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.jar.JarEntry;
 import java.util.jar.JarInputStream;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * @author lianglipeng.llp@alibaba-inc.com
@@ -80,8 +83,10 @@ public abstract class MatcherBaseTest {
 
     // 配置仓库地址
     private static final List<RemoteRepository> REPOSITORIES    = Arrays.asList(
-        new RemoteRepository.Builder("central", "default", "http://central.maven.org/maven2/")
-            .build());
+        new RemoteRepository.Builder("central", "default", "https://repo1.maven.org/maven2/")
+            .build(),
+        new RemoteRepository.Builder("aliyun", "default",
+            "https://maven.aliyun.com/repository/public/").build());
 
     // 本地仓库路径（默认 ~/.m2/repository）
     private static final String                 LOCAL_REPO_PATH = System.getProperty("user.home")
@@ -94,6 +99,180 @@ public abstract class MatcherBaseTest {
     public MatcherBaseTest() throws IOException {
     }
 
+    @Test
+    public void testGenSourceToPatch() throws Exception {
+        // 1. scan all java files from this adapter
+        String testRootPath = this.getClass().getClassLoader().getResource("").getFile();
+        String srcRootPath = Paths.get(testRootPath, "..", "..", "src", "main", "java")
+            .toAbsolutePath().toString();
+        List<AdapterPatch> adapterPatchs = convertAllJavaFiles(srcRootPath);
+
+        // 2. get all source from the maven central
+        List<org.eclipse.aether.artifact.Artifact> artifacts = downloadAllSources(config);
+        Assert.assertTrue("sources not found", CollectionUtils.isNotEmpty(artifacts));
+
+        for (AdapterPatch adapterPatch : adapterPatchs) {
+            String javaPackageAndName = adapterPatch.getSubPath() + "/"
+                                        + adapterPatch.getFileName();
+            for (org.eclipse.aether.artifact.Artifact artifact : artifacts) {
+                File sourceJarFile = artifact.getFile();
+                Map<String, byte[]> entryToContent = getFileContentAsLines(sourceJarFile,
+                    Pattern.compile("(.*\\.java$)"));
+                if (entryToContent.containsKey(javaPackageAndName)) {
+                    byte[] sourceContent = entryToContent.get(javaPackageAndName);
+                    List<String> sourceLines = edu.emory.mathcs.backport.java.util.Arrays
+                        .asList(new String(sourceContent).split("\\r?\\n"));
+                    adapterPatch.setSourceLines(sourceLines);
+                    break;
+                }
+            }
+        }
+
+        Path gitRoot = Files.createTempDirectory("koupleless-adapter");
+        Git git = Git.init().setDirectory(gitRoot.toFile()).call();
+        // 3. generate the patch files, 必须串行执行
+        for (AdapterPatch adapterPatch : adapterPatchs) {
+            List<String> adaptedLines = adapterPatch.getAdaptedLines();
+            List<String> sourceLines = adapterPatch.getSourceLines();
+
+            if (CollectionUtils.isEmpty(sourceLines) || CollectionUtils.isEmpty(adaptedLines)) {
+                continue;
+            }
+            List<String> patchContents = generateGitPatch(git, adapterPatch);
+            adapterPatch.setPatchLines(patchContents);
+        }
+
+        // 4. check the patch files works for the source files in all versions
+        for (AdapterPatch adapterPatch : adapterPatchs) {
+            String javaPackageAndName = adapterPatch.getSubPath() + "/"
+                                        + adapterPatch.getFileName();
+            for (org.eclipse.aether.artifact.Artifact artifact : artifacts) {
+                File sourceJarFile = artifact.getFile();
+                Map<String, byte[]> entryToContent = getFileContentAsLines(sourceJarFile,
+                    Pattern.compile("(.*\\.java$)"));
+                if (entryToContent.containsKey(javaPackageAndName)) {
+                    byte[] sourceContent = entryToContent.get(javaPackageAndName);
+                    List<String> sourceLines = edu.emory.mathcs.backport.java.util.Arrays
+                        .asList(new String(sourceContent).split("\\r?\\n"));
+                    adapterPatch.setSourceLines(sourceLines);
+                    try {
+                        checkGitPathValid(git, adapterPatch);
+                    } catch (GitAPIException e) {
+                        throw new RuntimeException(String.format(
+                            "this adapter not valid for %s:%s:%s, please download the %s:%s:%s sources jar and make a new adapter.",
+                            artifact.getGroupId(), artifact.getArtifactId(), artifact.getVersion(),
+                            artifact.getGroupId(), artifact.getArtifactId(), artifact.getVersion()),
+                            e);
+                    }
+                }
+            }
+        }
+    }
+
+    private List<AdapterPatch> convertAllJavaFiles(String srcRootPath) throws Exception {
+        List<Path> javaFiles = Files.walk(Paths.get(srcRootPath)).filter(Files::isRegularFile)
+            .filter(p -> p.toString().endsWith(".java")).collect(Collectors.toList());
+
+        // get all adapted sources from maven
+        List<AdapterPatch> adapterPatchs = new ArrayList<>();
+        for (Path javaFile : javaFiles) {
+            AdapterPatch adapterPatch = new AdapterPatch();
+            adapterPatch.setFileName(javaFile.getFileName().toString());
+            adapterPatch
+                .setSubPath(javaFile.getParent().toString().replaceFirst(srcRootPath + "/", ""));
+            adapterPatch.setRootPath(srcRootPath);
+            adapterPatch.setAdaptedLines(Files.readAllLines(javaFile));
+            adapterPatchs.add(adapterPatch);
+        }
+        return adapterPatchs;
+    }
+
+    private void checkGitPathValid(Git git, AdapterPatch adapterPatch) throws Exception {
+        Path gitRoot = git.getRepository().getDirectory().getParentFile().toPath();
+        List<String> patchLines = adapterPatch.getPatchLines();
+        Path filePath = Paths.get(gitRoot.toString(), adapterPatch.getSubPath(),
+            adapterPatch.getFileName());
+        Files.write(filePath, adapterPatch.getSourceLines());
+        ByteArrayInputStream patchStream = new ByteArrayInputStream(
+            patchLines.stream().collect(Collectors.joining("\n")).getBytes());
+        git.apply().setPatch(patchStream).call();
+        git.reset().addPath(adapterPatch.getSubPath() + "/" + adapterPatch.getFileName()).call();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        DiffFormatter formatter = new DiffFormatter(outputStream);
+        formatter.setRepository(git.getRepository());
+        formatter.setDiffComparator(RawTextComparator.WS_IGNORE_ALL);
+
+        // 获取 work 空间的内容
+        FileTreeIterator workTreeIterator = new FileTreeIterator(git.getRepository());
+        // 获取 head 的内容
+        CanonicalTreeParser headTreeParser = new CanonicalTreeParser(null,
+            git.getRepository().newObjectReader(), git.getRepository().resolve("HEAD^{tree}"));
+        List<DiffEntry> diffEntries = formatter.scan(headTreeParser, workTreeIterator);
+        for (DiffEntry diffEntry : diffEntries) {
+            formatter.format(diffEntry);
+            String diffContent = outputStream.toString();
+            List<String> diffLines = edu.emory.mathcs.backport.java.util.Arrays
+                .asList(diffContent.split("\\r?\\n"));
+            Assert.assertEquals("there should no diff here.", diffLines.size(), 4);
+            // reset outputStream
+            outputStream.reset();
+        }
+
+        git.checkout().addPath(adapterPatch.getSubPath() + "/" + adapterPatch.getFileName()).call();
+    }
+
+    private List<String> generateGitPatch(Git git, AdapterPatch adapterPatch) throws Exception {
+        // 1. 打开 /tmp 目录里临时目录
+        // 2. 拷贝 source 进去，然后 git init
+        Path gitRoot = git.getRepository().getDirectory().getParentFile().toPath();
+        Path sourcePath = Paths.get(gitRoot.toString(), adapterPatch.getSubPath());
+        Files.createDirectories(sourcePath);
+        Files.write(sourcePath.resolve(adapterPatch.getFileName()), adapterPatch.getSourceLines());
+        git.add().addFilepattern(".").call();
+        git.commit().setMessage("add source").call();
+        Files.write(sourcePath.resolve(adapterPatch.getFileName()), adapterPatch.getAdaptedLines());
+        git.add().addFilepattern(".").call();
+        git.commit().setMessage("add patched source").call();
+
+        Iterator<RevCommit> commits = git.log().call().iterator();
+        RevCommit newCommit = commits.next();
+        RevCommit oldCommit = commits.next();
+
+        ByteArrayOutputStream diffOutputStream = new ByteArrayOutputStream();
+        DiffFormatter formatter = new DiffFormatter(diffOutputStream);
+        formatter.setRepository(git.getRepository());
+        formatter.setDiffComparator(RawTextComparator.WS_IGNORE_ALL);
+        List<DiffEntry> diffEntries = formatter.scan(oldCommit.getTree(), newCommit.getTree());
+        for (DiffEntry diffEntry : diffEntries) {
+            formatter.format(diffEntry);
+        }
+
+        Path targetPath = Paths.get(adapterPatch.getRootPath(), "../resources",
+            adapterPatch.getSubPath(), adapterPatch.getFileName() + ".patch");
+        Files.createDirectories(targetPath.getParent());
+        byte[] bytePatchContent = diffOutputStream.toByteArray();
+        List<String> patchContent = edu.emory.mathcs.backport.java.util.Arrays
+            .asList(new String(bytePatchContent).split("\\r?\\n"));
+        Files.write(targetPath, bytePatchContent);
+        return patchContent;
+    }
+
+    private List<org.eclipse.aether.artifact.Artifact> downloadAllSources(KouplelessAdapterConfig config) throws Exception {
+        List<org.eclipse.aether.artifact.Artifact> artifacts = new ArrayList<>();
+        for (MavenDependencyAdapterMapping mapping : config.getAdapterMappings()) {
+            List<String> versions = parseArtifactVersion(mapping);
+            for (String version : versions) {
+                org.eclipse.aether.artifact.Artifact artifact = new DefaultArtifact(
+                    mapping.getMatcher().getGroupId(), mapping.getMatcher().getArtifactId(),
+                    "sources", "jar", version);
+
+                artifacts.add(resolveArtifacts(artifact).getArtifact());
+            }
+        }
+        return artifacts;
+    }
+
     // 解析依赖树
     protected ArtifactResult resolveArtifacts(org.eclipse.aether.artifact.Artifact artifact) throws ArtifactResolutionException {
         ArtifactRequest artifactRequest = new ArtifactRequest().setArtifact(artifact)
@@ -101,7 +280,7 @@ public abstract class MatcherBaseTest {
         return system.resolveArtifact(session, artifactRequest);
     }
 
-    protected String parseArtifactVersion(MavenDependencyAdapterMapping mapping) throws Exception {
+    protected List<String> parseArtifactVersion(MavenDependencyAdapterMapping mapping) throws Exception {
         VersionRangeRequest versionRangeRequest = new VersionRangeRequest()
             .setArtifact(
                 new DefaultArtifact(String.format("%s:%s:%s", mapping.getMatcher().getGroupId(),
@@ -110,20 +289,14 @@ public abstract class MatcherBaseTest {
 
         VersionRangeResult versionRangeResult = system.resolveVersionRange(session,
             versionRangeRequest);
-        Version rangeHighestVersion = versionRangeResult.getHighestVersion();
-        if (rangeHighestVersion == null) {
-            throw new VersionRangeResolutionException(versionRangeResult, "No version found");
-        }
-        return rangeHighestVersion.toString();
+        return versionRangeResult.getVersions().stream().map(Version::toString)
+            .collect(Collectors.toList());
     }
 
     // 初始化仓库系统
     private RepositorySystem newRepositorySystem() {
-        DefaultServiceLocator locator = MavenRepositorySystemUtils.newServiceLocator();
-        locator.addService(RepositoryConnectorFactory.class, BasicRepositoryConnectorFactory.class);
-        locator.addService(TransporterFactory.class, HttpTransporterFactory.class);
-        locator.addService(TransporterFactory.class, FileTransporterFactory.class);
-        return locator.getService(RepositorySystem.class);
+        RepositorySystemSupplier supplier = new RepositorySystemSupplier();
+        return supplier.get();
     }
 
     // 创建仓库会话
@@ -138,6 +311,7 @@ public abstract class MatcherBaseTest {
         String MAPPING_FILE = "conf/adapter-mapping.yaml";
         InputStream mappingConfigIS = this.getClass().getClassLoader()
             .getResourceAsStream(MAPPING_FILE);
+        Assert.assertNotNull(String.format("config %s not found.", MAPPING_FILE), mappingConfigIS);
         Yaml yaml = new Yaml();
         return yaml.loadAs(mappingConfigIS, KouplelessAdapterConfig.class);
     }
